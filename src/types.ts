611c0233c43055a08c23b174e472a1406e8d8cea
corpglory-dev <47055832+corpglory-dev@users.noreply.github.com>
2020-06-10 19:36:27 +0300

    Initial rendering (#2)

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,19 @@ export type HeatmapTimeSerie = TimeSerie;
 export type HeatmapOptions = Options;
 
 export { TickOrientation, TimeFormat };
+
+export type HeatmapData = {
+  x: string,
+  y: string,
+  value: number
+}
+
+export type ColorRange = {
+  min: string,
+  max: string
+}
+
+export type ValueRange = {
+  min: number,
+  max: number
+}
