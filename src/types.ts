b4451bfc9e881c669f1dd6f4d2e9da3ebb284a7b
corpglory-dev <47055832+corpglory-dev@users.noreply.github.com>
2020-06-10 19:06:46 +0300

    chartwerk boilerplate (#1)

---
+++ src/types.ts
@@ -0,0 +1,6 @@
+import { TimeSerie, Options, TickOrientation, TimeFormat } from '@chartwerk/base';
+
+export type HeatmapTimeSerie = TimeSerie;
+export type HeatmapOptions = Options;
+
+export { TickOrientation, TimeFormat };
